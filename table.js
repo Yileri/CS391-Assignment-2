62ab70149f321cb7e933a308c7177d1d877cb7ab
Yileri <yigit.ileri@ozu.edu.tr>
2023-03-21 17:24:00 +0300

    adding js function to html

---
+++ table.js
@@ -0,0 +1,12 @@
+let properties = [
+    {homeType: "Townhouse", state: "TX", room: 2, price: 120.000, livingSpace: 100},
+    {homeType: "House", state: "AL", room: 2, price: 275.000, livingSpace: 140},
+    {homeType: "Apartment", state: "OK", room: 3, price: 249.000, livingSpace: 180},
+    {homeType: "House", state: "GA", room: 4, price: 380.000, livingSpace: 200},
+    {homeType: "Apartment", state: "NY", room: 4, price: 500.000, livingSpace: 150},
+];
+
+function sort() {
+    alert("Test");
+}
+
