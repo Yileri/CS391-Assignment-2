1ab7cc7e8528656012ae700b53921609a8adbf7f
Yileri <yigit.ileri@ozu.edu.tr>
2023-03-22 11:24:56 +0300

    display table

---
+++ script.js
@@ -0,0 +1,82 @@
+let properties = [
+  {homeType: "Townhouse", state: "TX", room: 2, price: 120.000, livingSpace: 100},
+  {homeType: "House", state: "AL", room: 2, price: 275.000, livingSpace: 140},
+  {homeType: "Apartment", state: "OK", room: 3, price: 249.000, livingSpace: 180},
+  {homeType: "House", state: "GA", room: 4, price: 380.000, livingSpace: 200},
+  {homeType: "Apartment", state: "NY", room: 4, price: 500.000, livingSpace: 150},
+];
+
+function generateTable() {
+  const table = document.querySelector("table");
+
+  const thead = table.createTHead();
+  let row = thead.insertRow();
+
+  const th1 = document.createElement("th");
+  const text1 = document.createTextNode("Home Type");
+  th1.appendChild(text1);
+  row.appendChild(th1);
+
+  const th2 = document.createElement("th");
+  const text2 = document.createTextNode("State");
+  th2.appendChild(text2);
+  row.appendChild(th2);
+
+  const th3 = document.createElement("th");
+  const text3 = document.createTextNode("Room");
+  th3.appendChild(text3);
+  row.appendChild(th3);
+
+  const th4 = document.createElement("th");
+  const text4 = document.createTextNode("Price ($)");
+  th4.appendChild(text4);
+  row.appendChild(th4);
+
+  const th5 = document.createElement("th");
+  const text5 = document.createTextNode("Living Space (m^2)");
+  th5.appendChild(text5);
+  row.appendChild(th5);
+  
+  for(let i = 0; i < properties.length; i++) {
+    let row = table.insertRow();
+    
+    let homeTypeCell = row.insertCell(0);
+    let homeText = document.createTextNode(properties[i].homeType);
+    homeTypeCell.appendChild(homeText);
+    
+    let stateCell = row.insertCell(1);
+    let stateText = document.createTextNode(properties[i].state);
+    stateCell.appendChild(stateText);
+    
+    let roomCell = row.insertCell(2);
+    let roomText = document.createTextNode(properties[i].room);
+    roomCell.appendChild(roomText);
+    
+    let priceCell = row.insertCell(3);
+    let priceText = document.createTextNode(properties[i].price);
+    priceCell.appendChild(priceText);
+    
+    let livingSpaceCell = row.insertCell(4);
+    let spaceText = document.createTextNode(properties[i].livingSpace);
+    livingSpaceCell.appendChild(spaceText);
+  }
+}
+
+function sort() {
+  let selected = document.getElementById("sort");
+  if (selected.value == "Home Type") {
+    properties.sort((a, b) => {
+      if (a.homeType < b.homeType) {
+        return -1;
+      } else if (a.homeType > b.homeType) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  }
+  generateTable();
+}
+
+window.onload = generateTable;
+
