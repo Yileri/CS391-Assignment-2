1ec264b99949fdd2b996d2eca5083098f8b13df8
Yileri <yigit.ileri@ozu.edu.tr>
2023-03-24 22:30:00 +0300

    sorting the filtered table

--- script.js
+++ script.js
@@ -1,99 +1,67 @@
 let properties = [
-  {homeType: "Townhouse", state: "TX", room: 2, price: 120.000, livingSpace: 100},
-  {homeType: "House", state: "AL", room: 2, price: 275.000, livingSpace: 140},
-  {homeType: "Apartment", state: "OK", room: 3, price: 249.000, livingSpace: 180},
-  {homeType: "House", state: "GA", room: 4, price: 380.000, livingSpace: 200},
-  {homeType: "Apartment", state: "NY", room: 4, price: 500.000, livingSpace: 150},
+  { homeType: "Townhouse", state: "TX", room: 2, price: 120.000, livingSpace: 100 },
+  { homeType: "House", state: "AL", room: 2, price: 275.000, livingSpace: 140 },
+  { homeType: "Apartment", state: "OK", room: 3, price: 249.000, livingSpace: 180 },
+  { homeType: "House", state: "GA", room: 4, price: 380.000, livingSpace: 200 },
+  { homeType: "Apartment", state: "NY", room: 4, price: 500.000, livingSpace: 150 },
 ];
 
+let currentArray = properties;
+
 function generateTable() {
-  /*
-  const table = document.createElement("table");
-
-  const thead = table.createTHead();
-  let row = thead.insertRow();
-
-  const th1 = document.createElement("th");
-  const text1 = document.createTextNode("Home Type");
-  th1.appendChild(text1);
-  row.appendChild(th1);
-
-  const th2 = document.createElement("th");
-  const text2 = document.createTextNode("State");
-  th2.appendChild(text2);
-  row.appendChild(th2);
-
-  const th3 = document.createElement("th");
-  const text3 = document.createTextNode("Room");
-  th3.appendChild(text3);
-  row.appendChild(th3);
-
-  const th4 = document.createElement("th");
-  const text4 = document.createTextNode("Price ($)");
-  th4.appendChild(text4);
-  row.appendChild(th4);
-
-  const th5 = document.createElement("th");
-  const text5 = document.createTextNode("Living Space (m^2)");
-  th5.appendChild(text5);
-  row.appendChild(th5);
-  */
-
   const table = document.getElementById("propertyTable");
-  
-  for(let i = 0; i < properties.length; i++) {
+
+  for (let i = 0; i < properties.length; i++) {
     let row = table.insertRow();
-    
+
     let homeTypeCell = row.insertCell(0);
     let homeText = document.createTextNode(properties[i].homeType);
     homeTypeCell.appendChild(homeText);
-    
+
     let stateCell = row.insertCell(1);
     let stateText = document.createTextNode(properties[i].state);
     stateCell.appendChild(stateText);
-    
+
     let roomCell = row.insertCell(2);
     let roomText = document.createTextNode(properties[i].room);
     roomCell.appendChild(roomText);
-    
+
     let priceCell = row.insertCell(3);
     let priceText = document.createTextNode(properties[i].price);
     priceCell.appendChild(priceText);
-    
+
     let livingSpaceCell = row.insertCell(4);
     let spaceText = document.createTextNode(properties[i].livingSpace);
     livingSpaceCell.appendChild(spaceText);
   }
-  //const tableDiv = document.getElementById("table1");
-  //document.body.insertBefore(table, tableDiv);
 }
 
 function updateTable(array) {
   const table = document.getElementById("propertyTable");
 
-  for (let i = table.rows.length-1; i > 0; i--) {
+  for (let i = table.rows.length - 1; i > 0; i--) {
     document.getElementsByTagName("tr")[i].remove();
   }
 
-  for(let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
     let row = table.insertRow();
-    
+
     let homeTypeCell = row.insertCell(0);
     let homeText = document.createTextNode(array[i].homeType);
     homeTypeCell.appendChild(homeText);
-    
+
     let stateCell = row.insertCell(1);
     let stateText = document.createTextNode(array[i].state);
     stateCell.appendChild(stateText);
-    
+
     let roomCell = row.insertCell(2);
     let roomText = document.createTextNode(array[i].room);
     roomCell.appendChild(roomText);
-    
+
     let priceCell = row.insertCell(3);
     let priceText = document.createTextNode(array[i].price);
     priceCell.appendChild(priceText);
-    
+
     let livingSpaceCell = row.insertCell(4);
     let spaceText = document.createTextNode(array[i].livingSpace);
     livingSpaceCell.appendChild(spaceText);
@@ -103,7 +71,7 @@ function updateTable(array) {
 function sort() {
   let selected = document.getElementById("sort");
   if (selected.value == "Home Type") {
-    properties.sort((a, b) => {
+    currentArray.sort((a, b) => {
       if (a.homeType < b.homeType) {
         return -1;
       } else if (a.homeType > b.homeType) {
@@ -113,7 +81,7 @@ function sort() {
       }
     });
   } else if (selected.value == "Price") {
-    properties.sort((a, b) => {
+    currentArray.sort((a, b) => {
       if (a.price < b.price) {
         return -1;
       } else if (a.price > b.price) {
@@ -123,23 +91,40 @@ function sort() {
       }
     });
   }
-  updateTable(properties);
+  updateTable(currentArray);
 }
 
 function filter() {
   let minPrice = document.getElementById("min").value;
   let maxPrice = document.getElementById("max").value;
 
-  if (minPrice > maxPrice) {
+  if (!minPrice && !maxPrice) {
+    alert("Please fill at least one parameter");
+    return;
+  } else if ((minPrice > maxPrice && minPrice && maxPrice)) {
     alert("Minimum price cannot be higher than maximum price");
     return;
+  } else if (minPrice && !maxPrice) {
+    function minFilter(property) {
+      return property.price >= minPrice;
+    }
+
+    currentArray = properties.filter(minFilter);
+    updateTable(currentArray);
+  } else if (!minPrice && maxPrice) {
+    function maxFilter(property) {
+      return property.price <= maxPrice;
+    }
+
+    currentArray = properties.filter(maxFilter);
+    updateTable(currentArray);
   } else {
     function minMaxFilter(property) {
       return property.price >= minPrice && property.price <= maxPrice;
     }
 
-    let filtered = properties.filter(minMaxFilter);
-    updateTable(filtered);
+    currentArray = properties.filter(minMaxFilter);
+    updateTable(currentArray);
   }
 }
 
